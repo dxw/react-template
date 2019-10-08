af1aeefecea07d52b62cc74d917e1edea030e381
F <f@dxw.com>
2019-10-08 16:20:49 +0100

    Set TypeScript up to use strict typing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
-const Index = () => <h1>Welcome to dxw's React template!</h1>;
+import { NextPage } from "next";
+import React from "react";
+
+// TODO: Replace this with a real index page.
+const Index: NextPage = () => <h1>Welcome to dxw's React template!</h1>;
 
 export default Index;
