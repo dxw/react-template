877d50dba9b5cc1ebc6f206907573f89c8acfe76
F <f@dxw.com>
2019-10-10 14:12:49 +0100

    Use styled-jsx for styling components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,18 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps): JSX.Element => (
-  <div>{children}</div>
+  <div>
+    {children}
+    {/* TODO: Replace the contents of the `style` tag with any global styles, or remove it if it's not applicable. */}
+    <style jsx global>
+      {`
+        body {
+          font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+            "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+        }
+      `}
+    </style>
+  </div>
 );
 
 export default Layout;
