0418d2b066ac032d55a1c8542f93d3ffba547c65
F <f@dxw.com>
2019-10-10 15:18:48 +0100

    Use a custom document for setting global styles

---
+++ pages/_document.test.tsx
@@ -0,0 +1,42 @@
+/* eslint-env jest */
+import { advanceTo, clear } from "jest-date-mock";
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Document from "./_document";
+
+beforeAll(() => {
+  advanceTo(new Date("2019-01-01T00:00:00.000Z"));
+});
+
+afterAll(() => {
+  clear();
+});
+
+it("renders correctly", () => {
+  const component = renderer.create(
+    <Document
+      html="<div>test-html</div>"
+      __NEXT_DATA__={{
+        dataManager: "test-data-manager",
+        props: {},
+        page: "/test-page",
+        query: {},
+        buildId: "test-build"
+      }}
+      dangerousAsPath="/test-dangerous-as-path"
+      ampPath="/test-amp-path"
+      inAmpMode={false}
+      hybridAmp={false}
+      staticMarkup={false}
+      isDevelopment={false}
+      hasCssMode={false}
+      devFiles={[]}
+      files={[]}
+      dynamicImports={[]}
+      canonicalBase="http://localhost:3000/"
+    />
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
