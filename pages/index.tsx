66aff11891b1605056baa93733ca5c056ee29b05
F <f@dxw.com>
2019-10-10 16:01:58 +0100

    Set page titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import React from "react";
 
 import Layout from "../components/Layout";
@@ -6,6 +7,10 @@ import Layout from "../components/Layout";
 // TODO: Replace this with a real index page.
 const Index: NextPage = () => (
   <Layout>
+    <Head>
+      <title>React template - Index</title>
+    </Head>
+
     <h1>Welcome to dxw&apos;s React template!</h1>
   </Layout>
 );
