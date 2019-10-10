e6d06f797406bd74ca570a06d400ff25dedd7fbb
F <f@dxw.com>
2019-10-10 17:23:57 +0100

    Use a custom app for setting the default title

---
+++ pages/_app.test.tsx
@@ -0,0 +1,26 @@
+/* eslint-env jest */
+import { createRouter } from "next/router";
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App from "./_app";
+import Index from "./index";
+
+it("renders correctly", () => {
+  const component = renderer.create(
+    <App
+      pageProps={{}}
+      Component={Index}
+      router={createRouter("/test-path", {}, "test-as", {
+        subscription: jest.fn(),
+        initialProps: {},
+        pageLoader: jest.fn(),
+        Component: Index,
+        App,
+        wrapApp: jest.fn()
+      })}
+    />
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
