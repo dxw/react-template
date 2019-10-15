8c2358667d92659ced9dbbd9b58b3e671e99dfaa
F <f@dxw.com>
2019-10-15 16:10:45 +0100

    Make ESLint config DRYer

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/* eslint-env node */
+const sharedPlugins = ["react", "prettier"];
+const sharedRuleExtends = ["eslint:recommended", "plugin:react/recommended"];
+const sharedRulePrettierExtends = ["prettier", "prettier/react"];
+
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [...sharedPlugins],
+  extends: [...sharedRuleExtends, ...sharedRulePrettierExtends],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: [...sharedPlugins, "@typescript-eslint"],
+      extends: [
+        ...sharedRuleExtends,
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        ...sharedRulePrettierExtends,
+        "prettier/@typescript-eslint"
+      ]
+    }
+  ]
+};
