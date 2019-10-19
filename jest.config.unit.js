54a1f059c0c7b2324f311c37d7ee9f34adb39639
F <f@dxw.com>
2019-10-19 15:35:44 +0100

    Stop supporting tests outside of tests/
    We want to be able to reliably exclude them from the main tsconfig, but
    also include them in a test tsconfig. This is difficult to do while
    they are spread around and still support editors finding the relevant
    config.

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -1,17 +1,13 @@
 /* eslint-env node */
 module.exports = {
-  setupFilesAfterEnv: ["jest-axe/extend-expect", "<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: [
+    "jest-axe/extend-expect",
+    "<rootDir>/tests/jest.setup.ts"
+  ],
   transform: {
     "^.+\\.tsx?$": "babel-jest"
   },
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|steps|test).[jt]s?(x)"
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/.next/",
-    "<rootDir>/node_modules/",
-    "<rootDir>/tests/features/"
-  ],
+  testMatch: ["<rootDir>/tests/**/?(*.)+(spec|steps|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["<rootDir>/tests/features/"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"]
 };
