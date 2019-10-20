7fe4f568020ee0f45d2e7ca7824b60f10f3f84e1
F <f@dxw.com>
2019-10-20 12:21:58 +0100

    Switch to static classes for feature step functions

---
+++ tests/helpers/steps/Then.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { axe } from "jest-axe";
+import { DefineStepFunction } from "jest-cucumber";
+
+class Then {
+  static iShouldSeeXOnThePage(then: DefineStepFunction): void {
+    then(/^I should see "(.+)" on the page$/, async (content: string) => {
+      const element = await browser!.findElement({ tagName: "body" });
+
+      await expect(element.getText()).resolves.toContain(content);
+    });
+  }
+
+  static thePageShouldBeAccessible(then: DefineStepFunction): void {
+    then("the page should be accessible", async () => {
+      const document = await browser!.getPageSource();
+
+      expect(await axe(document)).toHaveNoViolations();
+    });
+  }
+
+  static thePageTitleShouldBeX(then: DefineStepFunction): void {
+    then(/^the page title should be "(.+)"$/, async (title: string) => {
+      await expect(browser!.getTitle()).resolves.toEqual(title);
+    });
+  }
+}
+
+export default Then;
--- tests/helpers/steps/when.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { DefineStepFunction } from "jest-cucumber";
-
-export const whenIVisitX = (when: DefineStepFunction): void => {
-  when(/^I visit (\/.*)$/, async (relativeUrl: string) => {
-    await browser!.getRelative(relativeUrl);
-  });
-};
