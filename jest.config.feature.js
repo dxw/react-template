dfffae813513d56f6660aec3d6526eda0d96d997
F <f@dxw.com>
2019-10-28 17:11:03 +0000

    Use ts-jest so tests get the full benefits of TypeScript
    `babel-jest` doesn't support type-checking, namespaces, const enums,
    declaration merging, legacy import / export, or caret type-casting with
    JSX.
    
    See https://kulshekhar.github.io/ts-jest/user/babel7-or-ts

--- jest.config.feature.js
+++ jest.config.feature.js
@@ -1,15 +1,21 @@
 /* eslint-env node */
+const babelConfig = require("./babel.config");
+
 module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
   setupFilesAfterEnv: [
     "jest-axe/extend-expect",
     "<rootDir>/__tests__/jest.setup.ts",
     "<rootDir>/__tests__/jest.setup.feature.ts"
   ],
-  transform: {
-    "^.+\\.tsx?$": "babel-jest"
-  },
   testMatch: [
     "<rootDir>/__tests__/features/**/?(*.)+(spec|steps|test).[jt]s?(x)"
   ],
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"]
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.jest.json",
+      babelConfig
+    }
+  }
 };
--- jest.config.unit.js
+++ jest.config.unit.js
@@ -1,5 +1,8 @@
 /* eslint-env node */
+const babelConfig = require("./babel.config");
+
 module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
   setupFilesAfterEnv: [
     "jest-axe/extend-expect",
     "<rootDir>/__tests__/jest.setup.ts"
@@ -7,10 +10,13 @@ module.exports = {
   moduleNameMapper: {
     "^.+\\.css$": "<rootDir>/__tests__/helpers/transformers/fileTransformer"
   },
-  transform: {
-    "^.+\\.tsx?$": "babel-jest"
-  },
   testMatch: ["<rootDir>/__tests__/**/?(*.)+(spec|steps|test).[jt]s?(x)"],
   testPathIgnorePatterns: ["<rootDir>/__tests__/features/"],
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"]
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.jest.json",
+      babelConfig
+    }
+  }
 };
